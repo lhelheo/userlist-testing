55fc2bd20ca243d6f1683eb271d68bc5e26632b1
Leonardo Oliveira Villalva <leonardo.oliveira.villalva@hotmail.com>
2024-10-10 11:49:31 -0300

    fix: login register

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,56 +1,60 @@
 import { prisma } from "../libs/prisma";
 import JWT from "jsonwebtoken";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export const register = async (req: any, res: any) => {
     if (req.body.username && req.body.password) {
         let { username, password } = req.body;
-
+        
         let hasUser = await prisma.user.findUnique({
             where: {
-                username: username
-            }
+                username,
+            },
         });
-        if(!hasUser) {
+        if (!hasUser) {
             let newUser = await prisma.user.create({
                 data: {
-                    username: username,
-                    password: password
-                }
+                    username,
+                    password,
+                },
             });
-
-            const token = JWT.sign({ id: newUser.id, username: newUser.username }, 
-                process.env.JWT_SECRET as string, { expiresIn: '2h' });
             
-            res.status(201).json({ id: newUser.id, token });
+            const token = JWT.sign(
+                { id: newUser.id, username: newUser.username },
+                process.env.JWT_SECRET as string,
+                { expiresIn: "1h" }
+            )
+            return res.status(201).json({ id: newUser.id, username: newUser.username, token });
         } else {
-            res.json({ error: "User already exists" });
+            return res.status(400).json({ message: "User already exists" });
         }
     }
-    res.json({ error: "Invalid data" });
+    return res.status(400).json({ message: "Invalid data" });
 }
-
 export const login = async (req: any, res: any) => {
-    if (req.body.username && req.body.password) {
-        let username: string = req.body.username;
-        let password: string = req.body.password;
-
+    if (req.body.username  && req.body.password) {
+        let { username, password } = req.body;
         let user = await prisma.user.findUnique({
-            where: { username, password }
+            where: {
+                username,
+            },
         });
-
         if (user) {
-            const token = JWT.sign({ id: user.id, username: user.username }, 
-            process.env.JWT_SECRET as string, { expiresIn: '2h' });
-            res.json({ status: true, token });
-            return;
+            if (user.password === password) {
+                const token = JWT.sign(
+                    { id: user.id, username: user.username },
+                    process.env.JWT_SECRET as string,
+                    { expiresIn: "1h" }
+                )
+                res.status(200).json({ status: true, token });
+            } else {
+                res.status(401).json({ message: "Invalid credentials" });
+            }
+        } else {
+            res.status(404).json({ message: "User not found" });
         }
     }
-    res.json({ status: false, error: "Invalid login" });
+    res.json({ status: false });
 }
-
 export const list = async (req: any, res: any) => {
     const users = await prisma.user.findMany();
     res.json(users);
