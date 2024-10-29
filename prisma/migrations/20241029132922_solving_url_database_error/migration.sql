8a1549ecdb4388ea081afa4e82c38ee1f1773bdb
Leonardo Oliveira Villalva <leonardo.oliveira.villalva@hotmail.com>
2024-10-29 10:29:47 -0300

    fix: database url

---
+++ prisma/migrations/20241029132922_solving_url_database_error/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createAt` on the `clients` table. All the data in the column will be lost.
+  - You are about to drop the column `updateAt` on the `clients` table. All the data in the column will be lost.
+  - You are about to drop the column `code` on the `products` table. All the data in the column will be lost.
+  - You are about to drop the column `selling_price` on the `products` table. All the data in the column will be lost.
+  - You are about to drop the column `createAt` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `updateAt` on the `users` table. All the data in the column will be lost.
+  - Added the required column `price` to the `products` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "clients" DROP COLUMN "createAt",
+DROP COLUMN "updateAt";
+
+-- AlterTable
+ALTER TABLE "products" DROP COLUMN "code",
+DROP COLUMN "selling_price",
+ADD COLUMN     "price" DOUBLE PRECISION NOT NULL,
+ADD COLUMN     "product_code" TEXT;
+
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "createAt",
+DROP COLUMN "updateAt";
