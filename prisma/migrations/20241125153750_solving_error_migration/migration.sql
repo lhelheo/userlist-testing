964fd4d4be09113877af8c0fce79c95c315e1cf0
Leonardo Oliveira Villalva <leonardo.oliveira.villalva@hotmail.com>
2024-11-25 12:43:08 -0300

    fix: schema prisma

---
+++ prisma/migrations/20241125153750_solving_error_migration/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "products" ALTER COLUMN "cost_price" DROP NOT NULL,
+ALTER COLUMN "cost_price" DROP DEFAULT;
---
+++ prisma/migrations/20241125154002_geration_by_price/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "products" ALTER COLUMN "remaining_balance" SET DEFAULT price;
